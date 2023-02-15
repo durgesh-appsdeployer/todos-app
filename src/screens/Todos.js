59d2ed301bc36e0dc96c362d25893c6ba9de4838
durgesh-appsdeployer <durgesh@appsdeployer.com>
2023-02-15 15:16:41 +0530

    Changed inline stylesheets

--- src/screens/Todos.js
+++ src/screens/Todos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import { useGetTodoQuery } from '../redux/services';
 
 export default function Todos() {
@@ -12,15 +12,14 @@ export default function Todos() {
   }
 
   return (
-    <View style={{ padding: "10%", paddingTop: "20%" }}>
-      <TouchableOpacity title="Upload File" style={{ height: 50, width: "100%", backgroundColor: "green", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", borderRadius: 10, elevation: 20 }} onPress={() => { setData(state.data) }}>
-        <Text style={{ color: "white", fontSize: 15 }}>Fetch Todos</Text>
+    <View style={styles.container}>
+      <TouchableOpacity title="Upload File" style={styles.button} onPress={() => { setData(state.data) }}>
+        <Text style={styles.buttonText}>Fetch Todos</Text>
       </TouchableOpacity>
-      <View style={{ height: 600, width: "100%", borderWidth: 1, borderColor: "lightgray", marginTop: 40, borderRadius: 5 }}>
+      <View style={styles.scrollContainer}>
         <FlatList nestedScrollEnabled={true} data={data}
           renderItem={(item) => {
-            console.log(item);
-            return (<View key={item.item.id} style={{ backgroundColor: item.item.completed ? "#b3f4cb" : "white", marginHorizontal: 7, marginVertical: 7, borderRadius: 5, paddingHorizontal: 20, paddingVertical: 15, elevation: 10, borderWidth: 1, borderColor: "lightgray" }}><Text key={item.item.id} style={{ color: "gray" }}>{item.item.title}</Text></View>)
+            return (<View key={item.item.id} style={item.item.completed ? styles.completeTodo : styles.incompleteTodo}><Text key={item.item.id} style={styles.todoText}>{item.item.title}</Text></View>)
           }} q
           keyExtractor={(item) => { item.index }}>
         </FlatList>
@@ -28,3 +27,58 @@ export default function Todos() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: "10%",
+    paddingTop: "20%"
+  },
+  button: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "green",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    elevation: 20
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 15
+  },
+  scrollContainer: {
+    height: 600,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "lightgray",
+    marginTop: 40,
+    borderRadius: 5
+  },
+  incompleteTodo: {
+    backgroundColor: "white",
+    marginHorizontal: 7,
+    marginVertical: 7,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    elevation: 10,
+    borderWidth: 1,
+    borderColor: "lightgray"
+  },
+  completeTodo: {
+    backgroundColor: "#b3f4cb",
+    marginHorizontal: 7,
+    marginVertical: 7,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    elevation: 10,
+    borderWidth: 1,
+    borderColor: "lightgray"
+  },
+  todoText: {
+    color: "gray"
+  }
+})
